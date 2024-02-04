1403816de0dbc00c1b5e3c70edd6664ba8c96476
yuri.silva.elite <yuri.silva.elite@gmail.com>
2024-02-04 15:08:49 -0300

    Atualizacao

--- Server/index.js
+++ Server/index.js
@@ -4,5 +4,13 @@ const app = express();
 
 app.get('/',(request,response)=> {
     return response.json ({message: 'Servidor is up' })
-    
 })
+
+app.post('/teste'),(request,response)=>{
+    const{name, data} = request.body;
+
+    return response.json({name,date});
+
+}
+
+app.listen(3333)
