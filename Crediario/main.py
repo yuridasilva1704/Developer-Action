27631f4ac77cda4cc1a1780c18a86a4e1217fd24
yuri.silva.elite <yuri.silva.elite@gmail.com>
2024-02-19 18:38:10 -0300

    Acrescentando botao

--- Crediario/main.py
+++ Crediario/main.py
@@ -1,17 +1,23 @@
-from kivymd.app import MDApp
-from kivymd.uix.label import MDLabel 
+from kivy.app import App
+from kivy.uix.button import Button
+from kivy.uix.label import Label 
 from kivy.core.window import Window
 
-class MainApp(MDApp):
+class ButtonTeste(App):
+    def build (self):
+        return Button(text='Logar')
 
-    def build(self):
-        Window.size = (300,600)
-        label = MDLabel(
-            text="E ae, Mundo!",            
+ButtonTeste().run()
+
+class MainApp(App):
+   def build(self):
+      Window.size = (300,600)
+      label = Label(
+          text="E ae, Mundo!",            
             halign = 'center',
             text_color=(0, 0, 0, 1),
             md_bg_color=(1, 1, 1, 1)
         )
-        return label
+      return label
 
 MainApp().run()
